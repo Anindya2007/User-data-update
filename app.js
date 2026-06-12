949954457706baabf99fabd01b015cece05ca1e0
Anindya Sarkar <sanindya710@gmail.com>
2026-06-12 21:52:44 +0530

    Create and read user features working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
+const userModels=require('./models/userModel')
+
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -12,8 +14,20 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/read', (req, res) => {
-    res.render('read')
+app.get('/read', async (req, res) => {
+    let allusers=await userModels.find();
+    res.render('read',{users:allusers});
+});
+
+
+app.post('/create', async (req, res) => {
+    let user=await userModels.create({
+        name:req.body.name,
+        email:req.body.email,
+        image:req.body.image
+    })
+    
+    res.redirect('/read');
 })
 
 app.listen(3000);
